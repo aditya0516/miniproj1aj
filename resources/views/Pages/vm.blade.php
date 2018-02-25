772ca5a559b10f4859b484067e7b76755d301ebc
aditya <avj22@njit.edu>
2018-02-25 02:41:06 -0500

    Almost done MP1

--- resources/views/Pages/vm.blade.php
+++ resources/views/Pages/vm.blade.php
@@ -2,23 +2,28 @@
 
 @section('content')
     <div>
-        <div class="card mt-3">
-    <table style="width:100%"  >
-        <tr>
-            <th>Name</th>
-            <th>Message</th>
-            <th> </th>
-        </tr>
-        <tr>
-            <td>Jill</td>
-            <td>My name is Jill </td>
-            <td><button type="submit" class="btn btn-primary col-lg-6">Delete</button></td>
-        </tr>
-        <tr>
-            <td>Eve</td>
-            <td>My name is Eve </td>
-            <td><button type="submit" class="btn btn-primary col-lg-6">Delete</button></td>
-        </tr>
-    </table>
-        </div></div>
+        <div class="mt-3">
+            <table style="width:100%">
+                <tr>
+                    <th>Name</th>
+                    <th>Message</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td>Jill</td>
+                    <td>My name is Jill</td>
+                    <td>
+                        <button type="submit" class="btn btn-primary col-lg-6">Delete</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Eve</td>
+                    <td>My name is Eve</td>
+                    <td>
+                        <button type="submit" class="btn btn-primary col-lg-6">Delete</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @endsection
