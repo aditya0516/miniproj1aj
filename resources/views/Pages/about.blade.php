4f8f9d3f6672ec7370402fa32a47ddcdd6538374
aditya <avj22@njit.edu>
2018-02-25 19:25:45 -0500

    Final Commit

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="mt-3">
         <h1>This is my About Page.</h1>
-        <p class="lead">This is my about page which tell you more about me.<br> All you get is this text and a
+        <p class="lead">This is my about page which tell you more about me. This is a demo about page depicting how about pages are supposed to be made.<br> All you get is this text and a
             mostly barebones HTML document.</p>
     </div>
 @endsection
